db40ce2e8c52177ef90f4e13d6ab21b28053de86
syb999 <13062678135@163.com>
2021-12-12 20:54:19 +0800

    python3: revert to 3.8.2

--- lang/python/python-host.mk
+++ lang/python/python-host.mk
@@ -1,28 +1,47 @@
 #
-# Copyright (C) 2017 OpenWrt.org
+# Copyright (C) 2015-2016 OpenWrt.org
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
 
-# Note: include this file after `include $(TOPDIR)/rules.mk in your package Makefile
-#       if `python3-package.mk` is included, this will already be included
+# Note: include this after `include $(TOPDIR)/rules.mk in your package Makefile
+#       if `python-package.mk` is included, this will already be included
 
-# For PYTHON3_VERSION
-python3_mk_path:=$(dir $(lastword $(MAKEFILE_LIST)))
-include $(python3_mk_path)python3-version.mk
+ifneq ($(__python_host_mk_inc),1)
+__python_host_mk_inc=1
 
-HOST_PYTHON3_DIR:=$(STAGING_DIR_HOSTPKG)
-HOST_PYTHON3_INC_DIR:=$(HOST_PYTHON3_DIR)/include/python$(PYTHON3_VERSION)
-HOST_PYTHON3_LIB_DIR:=$(HOST_PYTHON3_DIR)/lib/python$(PYTHON3_VERSION)
+# For PYTHON_VERSION
+python_mk_path:=$(dir $(lastword $(MAKEFILE_LIST)))
+include $(python_mk_path)python-version.mk
 
-HOST_PYTHON3_PKG_DIR:=$(HOST_PYTHON3_DIR)/lib/python$(PYTHON3_VERSION)/site-packages
+HOST_PYTHON_DIR:=$(STAGING_DIR_HOSTPKG)
+HOST_PYTHON_INC_DIR:=$(HOST_PYTHON_DIR)/include/python$(PYTHON_VERSION)
+HOST_PYTHON_LIB_DIR:=$(HOST_PYTHON_DIR)/lib/python$(PYTHON_VERSION)
 
-HOST_PYTHON3_BIN:=$(HOST_PYTHON3_DIR)/bin/python$(PYTHON3_VERSION)
+HOST_PYTHON_PKG_DIR:=$(HOST_PYTHON_DIR)/lib/python$(PYTHON_VERSION)/site-packages
 
-HOST_PYTHON3PATH:=$(HOST_PYTHON3_LIB_DIR):$(HOST_PYTHON3_PKG_DIR)
+HOST_PYTHON_BIN:=$(HOST_PYTHON_DIR)/bin/python$(PYTHON_VERSION)
 
-HOST_PYTHON3_VARS = \
+HOST_PYTHONPATH:=$(HOST_PYTHON_LIB_DIR):$(HOST_PYTHON_PKG_DIR)
+
+define HostPython
+	if [ "$(strip $(3))" == "HOST" ]; then \
+		export PYTHONPATH="$(HOST_PYTHONPATH)"; \
+		export PYTHONDONTWRITEBYTECODE=0; \
+	else \
+		export PYTHONPATH="$(PYTHONPATH)"; \
+		export PYTHONDONTWRITEBYTECODE=1; \
+		export _python_sysroot="$(STAGING_DIR)"; \
+		export _python_prefix="/usr"; \
+		export _python_exec_prefix="/usr"; \
+	fi; \
+	export PYTHONOPTIMIZE=""; \
+	$(1) \
+	$(HOST_PYTHON_BIN) $(2);
+endef
+
+define host_python_settings
 	ARCH="$(HOST_ARCH)" \
 	CC="$(HOSTCC)" \
 	CCSHARED="$(HOSTCC) $(HOST_FPIC)" \
@@ -30,53 +49,47 @@ HOST_PYTHON3_VARS = \
 	LD="$(HOSTCC)" \
 	LDSHARED="$(HOSTCC) -shared" \
 	CFLAGS="$(HOST_CFLAGS)" \
-	CPPFLAGS="$(HOST_CPPFLAGS) -I$(HOST_PYTHON3_INC_DIR)" \
-	LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON3_VERSION) -Wl$(comma)-rpath$(comma)$(STAGING_DIR_HOSTPKG)/lib" \
-	PYTHONPATH="$(HOST_PYTHON3PATH)" \
-	PYTHONDONTWRITEBYTECODE=0 \
-	PYTHONOPTIMIZE=""
+	CPPFLAGS="$(HOST_CPPFLAGS) -I$(HOST_PYTHON_INC_DIR)" \
+	LDFLAGS="$(HOST_LDFLAGS) -lpython$(PYTHON_VERSION) -Wl$(comma)-rpath=$(STAGING_DIR_HOSTPKG)/lib" \
+	_PYTHON_HOST_PLATFORM=linux2
+endef
 
-# $(1) => directory of python script
+# $(1) => commands to execute before running pythons script
 # $(2) => python script and its arguments
 # $(3) => additional variables
-define HostPython3/Run
-	cd "$(if $(strip $(1)),$(strip $(1)),.)" && \
-	$(HOST_PYTHON3_VARS) \
-	$(3) \
-	$(HOST_PYTHON3_BIN) $(2)
+define Build/Compile/HostPyRunHost
+	$(call HostPython, \
+		$(if $(1),$(1);) \
+		$(call host_python_settings) \
+		$(3) \
+		, \
+		$(2) \
+		, \
+		HOST \
+	)
 endef
 
 # Note: I shamelessly copied this from Yousong's logic (from python-packages);
-HOST_PYTHON3_PIP:=$(STAGING_DIR_HOSTPKG)/bin/pip$(PYTHON3_VERSION)
+HOST_PYTHON_PIP:=$(STAGING_DIR_HOSTPKG)/bin/pip$(PYTHON_VERSION)
 
-HOST_PYTHON3_PIP_CACHE_DIR:=$(DL_DIR)/pip-cache
-
-# Multiple concurrent pip processes can lead to errors or unexpected results: https://github.com/pypa/pip/issues/2361
 # $(1) => packages to install
-define HostPython3/PipInstall
-	$(call locked, \
-		$(HOST_PYTHON3_VARS) \
-		$(HOST_PYTHON3_PIP) \
-			--cache-dir "$(HOST_PYTHON3_PIP_CACHE_DIR)" \
-			--disable-pip-version-check \
-			install \
-			--no-binary :all: \
-			--require-hashes \
-			$(1) \
-		$(if $(CONFIG_PYTHON3_HOST_PIP_CACHE_WORLD_READABLE), \
-			&& $(FIND) $(HOST_PYTHON3_PIP_CACHE_DIR) -not -type d -exec chmod go+r  '{}' \; \
-			&& $(FIND) $(HOST_PYTHON3_PIP_CACHE_DIR)      -type d -exec chmod go+rx '{}' \; \
-		), \
-		pip \
-	)
+define Build/Compile/HostPyPipInstall
+	$(call host_python_settings) \
+	$(HOST_PYTHON_PIP) \
+		--disable-pip-version-check \
+		--cache-dir "$(DL_DIR)/pip-cache" \
+		install \
+		$(1)
 endef
 
 # $(1) => build subdir
 # $(2) => additional arguments to setup.py
 # $(3) => additional variables
-define HostPython3/ModSetup
-	$(call HostPython3/Run, \
-		$(HOST_BUILD_DIR)/$(strip $(1)), \
-		setup.py $(2), \
+define Build/Compile/HostPyMod
+	$(call Build/Compile/HostPyRunHost, \
+		cd $(HOST_BUILD_DIR)/$(strip $(1)), \
+		./setup.py $(2), \
 		$(3))
 endef
+
+endif # __python_host_mk_inc
